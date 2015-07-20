0acd12931ff0359f7c4fbc994cc14586bdf28a1b
SamC <sam.coward@ntlworld.com>
2015-07-20 17:32:07 +0100

    Edits to gitignore and added rmbuild script

---
+++ rmbuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script removes all build files from project
+# CAUTION: only run the script in project's root folder
+
+rm -r core/build
+rm -r desktop/build
+rm -r android/build
+rm -r ios/build
+rm -r html/build
