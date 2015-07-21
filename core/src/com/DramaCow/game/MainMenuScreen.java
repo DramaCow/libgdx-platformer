62295e9770f400252a2eec110c4431dceff15813
Marky96 <markelkommos@Marks-MacBook-Pro.local>
2015-07-21 12:41:22 +0100

    Adding a goToGame button from menu screen

--- core/src/com/DramaCow/game/MainMenuScreen.java
+++ core/src/com/DramaCow/game/MainMenuScreen.java
@@ -5,11 +5,14 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class MainMenuScreen extends ScreenAdapter {
 
 	GDXgame game;
 	OrthographicCamera cam;
+	Rectangle goToGameScreen;
+	Vector3 touchPoint;
 
 	public MainMenuScreen (GDXgame game) {
 		this.game = game;
@@ -18,10 +21,19 @@ public class MainMenuScreen extends ScreenAdapter {
 		cam = new OrthographicCamera(w, h);
 		cam.position.set(cam.viewportWidth / 2.0f, cam.viewportHeight / 2.0f, 0.0f);
 		cam.update();
+		touchPoint = new Vector3();
+		goToGameScreen = new Rectangle(20, 100, 50, 50);
 	}
 
 
 	public void update() {
+		if(Gdx.input.justTouched()){
+			guiCam.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
+
+			if(goToGameScreen.contains(touchPoint.x, touchPoint.y)){
+				game.setScreen(new GameScreen(game));
+			}
+		}
 		
 	}
 
@@ -45,7 +57,7 @@ public class MainMenuScreen extends ScreenAdapter {
 	public void resize(int width, int height) {
 		cam.viewportWidth = (float) width;
 		cam.viewportHeight = (float) height;
-			cam.position.set(cam.viewportWidth / 2.0f, cam.viewportHeight / 2.0f, 0.0f);
+		cam.position.set(cam.viewportWidth / 2.0f, cam.viewportHeight / 2.0f, 0.0f);
 		cam.update();
 	}
 
