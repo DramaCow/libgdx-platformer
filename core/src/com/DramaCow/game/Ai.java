b7e95b8f2c671f2de1b83b7ee342083d0835926e
Josh Anthony <shadowsora@live.co.uk>
2015-07-22 16:18:10 +0100

    Added Enemy and AI interface. Small adjustments to other classes I made earlier. Added crappy temp sprites to test when rendering.

---
+++ core/src/com/DramaCow/game/Ai.java
@@ -0,0 +1,7 @@
+package com.DramaCow.game;
+
+public interface Ai {
+
+	public void update(Enemy enemy, Float deltaTime);
+
+}
--- core/src/com/DramaCow/game/Player.java
+++ core/src/com/DramaCow/game/Player.java
@@ -8,36 +8,37 @@ public class Player extends DynamicGameObject{
 		RUN, JUMP, FALL, ATTACK, HURT, DEAD
 	}
 
-	public static final float PLAYER_WIDTH = 32;
-	public static final float PLAYER_HEIGHT = 32;
+	public static final float PLAYER_WIDTH = 32.0f;
+	public static final float PLAYER_HEIGHT = 48.0f;
 
 	private PlayerState state;
 	private int health;
 	private float timeInState;
 
 	public Player(float x, float y){
-		super(x,y,PLAYER_WIDTH,PLAYER_HEIGHT);
-		state = PlayerState.RUN;
+		super("Player",x,y,PLAYER_WIDTH,PLAYER_HEIGHT);
+		setState(PlayerState.RUN); 
 	}
 
 	@Override
 	public void update(float deltaTime){
-		velocity.add(accel.x * deltaTime, accel.y * deltaTime);
-		position.add(velocity.x * deltaTime, velocity.y * deltaTime);
-		bounds.x = position.x;
-		bounds.y = position.y;
+		super.update(deltaTime);
 		timeInState += deltaTime;
+
+		// If enough time passed leave hurt state
+		if(state == PlayerState.HURT && timeInState > 2.0f) setState(PlayerState.RUN); 
 	}
 
 	public void hurt(int damage){
 		health -= damage;
-		state = PlayerState.HURT;
+		setState(PlayerState.HURT); 
 	}
 
 	public void heal(int amount){
 		health += amount;
 	}
 
+	//Sets state a resets state timer
 	private void setState (PlayerState s){
 		state = s;
 		timeInState = 0;
