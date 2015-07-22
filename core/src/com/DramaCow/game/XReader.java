a7db6cbfd07945053715d0ab17ecd758e70ca518
Marky96 <markelkommos@Marks-MacBook-Pro.local>
2015-07-22 16:17:03 +0100

    Added xml files for each level, and created function to return xml file names

--- core/src/com/DramaCow/game/XReader.java
+++ core/src/com/DramaCow/game/XReader.java
@@ -1,5 +1,10 @@
 package com.DramaCow.game;
 
+import java.util.Iterator;
+import com.badlogic.gdx.utils.Array;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 import com.badlogic.gdx.utils.XmlReader;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.XmlReader.Element;
@@ -11,17 +16,44 @@ public class XReader {
 	
 	private XReader() {}
 
-	public static void parsing(String filename){
+	public static XmlReader.Element parsing(String filename){
+		XmlReader.Element root = null;
 		try{
 			XmlReader reader = new XmlReader();
-			System.out.println(Gdx.files.internal(filename));
-			XmlReader.Element e = reader.parse(Gdx.files.internal(filename));
-
-			System.out.println(e.getName());
+			root = reader.parse(Gdx.files.internal(filename));
 		}	
 		catch(Exception e){
 			System.out.println(e);
 		}
+
+		return root;
 	}
+
+	/*public static Map<Integer, String> getMap(String filename){
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		XmlReader.Element root = parsing(filename);
+		Array<Element> levels = root.getChildrenByName("levels");
+
+		System.out.println(levels.first().getChildCount());
+		for(int i = 0; i<levels.first().getChildCount(); i++){
+     		XmlReader.Element level_element = levels.first().getChild(i);
+     		Integer level_number = Integer.parseInt(level_element.getAttribute("level_number"));
+     		String level_name = level_element.getName();
+     		System.out.println(level_name);
+     		System.out.println(level_number);
+     		map.put(level_number, level_name);
+ 		}
+
+		return map;
+	}*/
+
+	public static String getFileName(String filename, String levelname){
+		XmlReader.Element root = parsing(filename);
+		Array<Element> levels = root.getChildrenByName("levels");
+		Array<Element> level = levels.first().getChildrenByName(levelname);
+		return level.get(0).getAttribute("level_file");
+	}
+
+
 
 }
